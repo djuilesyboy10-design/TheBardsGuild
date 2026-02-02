10a07513b5e244287a2fe0cac95fbceceb3d3c80
djuil <djuil@users.noreply.github.com>
2026-02-02 22:49:11 +0000

    Removed final backup esp folder - ultimate cleanup

--- back up esp/.backupengine/framework/npc_click_activator_player.lua
+++
@@ -1,8 +0,0 @@
--- NPC Click Activator Player Script
--- Player-side component for NPC click activation
--- Currently empty as global script handles all logic
-
-return {
-    -- Player event handlers can be added here if needed
-    -- For now, global script handles all NPC click activation
-}
--- back up esp/RESTORE_POINT_Universal_Framework_Complete/Lua/engine/plugins/ballad/ballad_event_listener.lua
+++
@@ -1,8 +0,0 @@
-return {
-    eventHandlers = {
-        Bard_JiubBallad_Event = function()
-            print("[BARD][EVENT] Jiub Ballad event received")
-            require("scripts.engine.plugins.ballad.ballad_helper").show()
-        end
-    }
-}
